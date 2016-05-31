6aa3da8fa6bb4a03ff67dededf279efc14103d85
Chandra Akella - Contractor <akellac@akellac-mac.alsac.local>
2016-05-31 10:15:42 -0500

    Minor Changes.

--- scripts/app.js
+++ scripts/app.js
@@ -4,7 +4,7 @@ var app = angular.module('app',['ngRoute']);
 //Controllers
 
 app.controller('pageCtrl',['$scope','$http', '$sce', function($scope,$http, $sce){
-    $http.get('http://stj2dev.convio.net/site/TR?pg=personal&fr_id=19412&px=2094527')
+    $http.get('https://rawgit.com/chandraakella29/3c1060d8ed642f35e6e46b1eee74c17e/raw/167ff9930eea875bd579bf5aeed84a57c5ae8be6/index.html')
             .success(function (response) {
                  console.log('response is', response);
                  $scope.htmlResponse = $sce.trustAsHtml(response);
